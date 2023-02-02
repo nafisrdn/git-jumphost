d52d07457fe331a6589ce5159a4fb43f43ed2ab1
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-02 15:06:30 +0700

    third

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const http = require("http");
 const exec = require("child_process").exec;
 
-const sourceRepoUrl = "https://gitlab.com/nafisrdn/webhook-test.git";
-const targetRepoUrl = "https://gitlab.com/nafisrdn/test-git-2.git";
+const port = 8000;
 
-const branch = "main";
+const sourceRepoUrl = "https://gitlab.com/nafisrdn/webhook-test.git";
+const targetRepoUrl = "https://gitlab.com/nafisrdn/test-git-3.git";
+
+const sourceBranch = "main";
+const targetBranch = "main";
 
 const runGitCommand = (command) =>
   new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
+    exec(command, { cwd: "repo" }, (error, stdout, stderr) => {
       if (error) {
         reject(error);
       }
@@ -17,27 +20,30 @@ const runGitCommand = (command) =>
   });
 
 const gitPullFromSource = async () => {
-  const pullCommand = `git --git-dir=repo/.git pull ${sourceRepoUrl} ${branch}`;
+  const pullCommand = `git pull ${sourceRepoUrl} ${sourceBranch}`;
   const pullResult = await runGitCommand(pullCommand);
   console.log(pullResult);
 };
 
 const executeRestoreGit = async () => {
-  const restoreCommand = `git --git-dir=repo/.git --work-tree=repo checkout .`;
+  const restoreCommand = `git checkout .`;
   const restoreResult = await runGitCommand(restoreCommand);
   console.log(restoreResult);
 };
 
 const gitPushToTarget = async () => {
-  const pushCommand = `git --git-dir=repo/.git push ${targetRepoUrl} ${branch}`;
+  const pushCommand = `git push ${targetRepoUrl} ${sourceBranch}:${targetBranch}`;
   const pushResult = await runGitCommand(pushCommand);
   console.log(pushResult);
 };
+
+let taskCount = 0;
 
 http
   .createServer(async (req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
 
+    console.log(`======= ${taskCount} =======`);
     console.log("Discard local changes...");
     await executeRestoreGit();
     console.log("Discard done");
@@ -46,7 +52,20 @@ http
     await gitPullFromSource();
     console.log("Pull done");
 
+    console.log("Discard local changes...");
+    await executeRestoreGit();
+    console.log("Discard done");
+
+    console.log("Pushing to target...");
+    await gitPushToTarget();
+    console.log("Push done");
+    console.log(`======= = =======`);
+
+    taskCount++;
+
     res.write("ok");
     res.end();
   })
-  .listen(8000);
+  .listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+target_repo_url=https://gitlab.com/nafisrdn/webhook-test.git
+
+git clone $target_repo_url ./repo
