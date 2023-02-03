43060198f7631c8ebc839aa7fd7bbec240d037db
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-03 18:31:51 +0700

    Fix logger multiple line
    Fix the logger message from multiple line to inline by transforming the message using trim and replace

--- src/utils/logger.utils.js
+++ src/utils/logger.utils.js
@@ -3,14 +3,13 @@ const winston = require("winston");
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
-    winston.format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss",
-    }),
-    winston.format.printf((info) => {
-      return `[${info.timestamp}] ${info.level}: ${info.message}`;
-    })
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.printf(
+      ({ message, timestamp, level }) =>
+        `[${timestamp}] ${level}: ${message.replace(/\n/g, " ").trim()}`
+    )
   ),
   transports: [new winston.transports.Console()],
 });
 
-module.exports.logger = logger;
+module.exports = { logger };
