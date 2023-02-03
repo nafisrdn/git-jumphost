0210ddefc0d7be4eff6fd03d2f7eb3afffceefb3
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-03 18:54:41 +0700

    Fix log error trace not showing

--- src/utils/logger.utils.js
+++ src/utils/logger.utils.js
@@ -4,10 +4,16 @@ const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
     winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    winston.format.printf(
-      ({ message, timestamp, level }) =>
-        `[${timestamp}] ${level}: ${message.replace(/\n/g, " ").trim()}`
-    )
+    winston.format.printf((info) => {
+      const { message, timestamp, level } = info;
+
+      let transformedMessage = message.replace(/\n/g, " ").trim();
+
+      if (info instanceof Error) {
+        transformedMessage = `${info.message} ${info.stack}`;
+      }
+      return `[${timestamp}] ${level}: ${transformedMessage}`;
+    })
   ),
   transports: [new winston.transports.Console()],
 });
