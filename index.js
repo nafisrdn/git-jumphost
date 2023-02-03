5083caa21c0656bf6f2dacabd7534b7afc6f97b0
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-03 11:49:46 +0700

    Add http error handler

--- index.js
+++ index.js
@@ -12,29 +12,37 @@ let taskCount = 0;
 
 http
   .createServer(async (req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
+    try {
+      res.writeHead(200, { "Content-Type": "text/plain" });
 
-    console.log(`======= ${taskCount} =======`);
-    console.log("Discard local changes...");
-    await executeRestoreGit();
-    console.log("Discard done");
+      console.log(`======= ${taskCount} =======`);
+      console.log("Discard local changes...");
+      await executeRestoreGit();
+      console.log("Discard done");
 
-    console.log("Pulling from source...");
-    await gitPullFromSource();
-    console.log("Pull done");
+      console.log("Pulling from source...");
+      await gitPullFromSource();
+      console.log("Pull done");
 
-    console.log("Discard local changes...");
-    await executeRestoreGit();
-    console.log("Discard done");
+      console.log("Discard local changes...");
+      await executeRestoreGit();
+      console.log("Discard done");
 
-    console.log("Pushing to target...");
-    await gitPushToTarget();
-    console.log("Push done");
-    console.log(`======= = =======`);
+      console.log("Pushing to target...");
+      await gitPushToTarget();
+      console.log("Push done");
+      console.log(`======= = =======`);
+
+      res.write("ok");
+    } catch (error) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+
+      console.log(error);
+
+      res.write(error);
+    }
 
     taskCount++;
-
-    res.write("ok");
     res.end();
   })
   .listen(PORT, () => {
