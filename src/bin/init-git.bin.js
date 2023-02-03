d1c0e4c5673ea37bb8dd89ca14c4d2d16c90c570
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-03 09:07:36 +0700

    update git clone init to remove directory if exist

--- src/bin/init-git.bin.js
+++ src/bin/init-git.bin.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+
 const path = require("path");
+const fs = require("fs");
 
 const {
   SOURCE_GIT_USERNAME,
@@ -11,14 +13,24 @@ const {
   runGitCommand,
 } = require("../services/git.service");
 
+const repoDirPath = path.join(__dirname, "../../repo");
+
 const cloneRepo = async () => {
+  const isDirectoryExist = fs.existsSync(repoDirPath);
+
+  if (isDirectoryExist) {
+    console.log("Removing existing repo directory...");
+    fs.rmSync(repoDirPath, { recursive: true, force: true });
+    console.log("Remove done");
+  }
+
   const generatedUrl = generateOriginUrlWithCreds(
     SOURCE_GIT_USERNAME,
     SOURCE_GIT_PASSWORD,
     SOURCE_REPO_URL
   );
 
-  const repoDirectory = path.join(__dirname, "../../repo");
+  const repoDirectory = repoDirPath;
 
   const cloneCommand = `git clone ${generatedUrl} ${repoDirectory}`;
 
