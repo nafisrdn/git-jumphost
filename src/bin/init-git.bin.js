1d44c10a44adcc1939b12c73764e8e418aaf5c40
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-07 17:07:54 +0700

    Switch from shell script to Git SDK for initializing Git repository
    Previously, the bin used a shell script to initialize a Git repository, but this has been replaced with the Git SDK for a more reliable and portable solution. The Git SDK provides a programmatic interface to Git, making it easier to automate Git operations in JavaScript. This change should improve the consistency and stability of the bin.

--- src/bin/init-git.bin.js
+++ src/bin/init-git.bin.js
@@ -10,30 +10,47 @@ const {
 } = require("../config/app.config");
 const { generateOriginUrlWithCreds } = require("../services/git.service");
 const { logger } = require("../utils/logger.utils");
-const { runGitCommand } = require("../utils/git.utils");
+
+const { simpleGit, CleanOptions } = require("simple-git");
+simpleGit().clean(CleanOptions.FORCE);
 
 const repoDirPath = path.join(__dirname, "../../repo");
 
-const cloneRepo = async () => {
+const createRepoDirectory = async () => {
+  logger.info(
+    `Creating empty directory for local repository in ${repoDirPath}`
+  );
+
+  await fs.promises.mkdir(repoDirPath);
+
+  logger.info(`Created empty directory for local repository`);
+};
+
+const replaceDirectoryIfExist = async () => {
   const isDirectoryExist = fs.existsSync(repoDirPath);
 
   if (isDirectoryExist) {
     logger.info(`Removing existing local repository from ${repoDirPath}`);
-    fs.rmSync(repoDirPath, { recursive: true, force: true });
+    await fs.promises.rm(repoDirPath, { recursive: true, force: true });
     logger.info(`Local repository removal successful`);
   }
+};
 
-  const generatedUrl = generateOriginUrlWithCreds(
+const cloneRepo = async () => {
+  await replaceDirectoryIfExist();
+  await createRepoDirectory();
+
+  const git = simpleGit(repoDirPath);
+
+  const sourceRemote = generateOriginUrlWithCreds(
     SOURCE_GIT_USERNAME,
     SOURCE_GIT_PASSWORD,
     SOURCE_REPO_URL
   );
 
-  const cloneCommand = `git clone ${generatedUrl} ${repoDirPath}`;
+  const cloneResult = await git.clone(sourceRemote, "./repo");
 
-  const clonseResult = await runGitCommand(cloneCommand, false);
-
-  return clonseResult;
+  return cloneResult;
 };
 
 (async () => {
