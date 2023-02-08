4662add6facba5f539b638313db160300cc78f4f
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-08 16:31:51 +0700

    Remove unused config variables
    Removed repo  branch config variables

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -5,9 +5,6 @@ const path = require("path");
 module.exports.SOURCE_REPO_URL = process.env.SOURCE_REPO_URL;
 module.exports.TARGET_REPO_URL = process.env.TARGET_REPO_URL;
 
-module.exports.SOURCE_BRANCH = process.env.SOURCE_BRANCH;
-module.exports.TARGET_BRANCH = process.env.TARGET_BRANCH;
-
 module.exports.SOURCE_GIT_USERNAME = process.env.SOURCE_GIT_USERNAME;
 module.exports.SOURCE_GIT_PASSWORD = process.env.SOURCE_GIT_PASSWORD;
 
