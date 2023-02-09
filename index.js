8261b2b7963fcb7b1bcd6b4d2042008b168129a1
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-09 14:29:02 +0700

    Update app version

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const handleRequest = async (req, res) => {
 
 const startServer = () => {
   http.createServer(handleRequest).listen(appConfig.PORT, () => {
-    logger.info("Jumphost version 1.2.1");
+    logger.info("Jumphost version 1.2.2");
     logger.info(`Server is listening on port ${appConfig.PORT}`);
   });
 };
