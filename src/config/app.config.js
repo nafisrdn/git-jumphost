658fdcb974d759aff1dfe57739bb5bfe8bf88ff3
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-15 09:39:13 +0700

    Update app version to app.config

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -2,3 +2,4 @@ module.exports.PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 module.exports.ENVIRONMENT = process.env.ENVIRONMENT || "DEV";
 module.exports.LOGS_DIRECTORY =
   process.env.LOGS_DIRECTORY || path.join(__dirname, "../../logs");
+module.exports.VERSION = "1.2.4";
