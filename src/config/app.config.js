a7dbe3978695176583de711db4e59d80942f2b36
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-17 15:10:17 +0700

    Fix path not defined in app config

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,4 +1,6 @@
 module.exports.PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+const path = require("path");
+
 module.exports.ENVIRONMENT = process.env.ENVIRONMENT || "DEV";
 module.exports.LOGS_DIRECTORY =
   process.env.LOGS_DIRECTORY || path.join(__dirname, "../../logs");
