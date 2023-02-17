4ab3fd239c0e308fbcf2f1497c42f49c312c7ebd
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-17 15:09:57 +0700

    Reorganize logger utils file for easier unit testing

--- src/utils/logger.util.js
+++ src/utils/logger.util.js
@@ -2,29 +2,34 @@ const winston = require("winston");
 const appConfig = require("../config/app.config");
 const DailyRotateFile = require("winston-daily-rotate-file");
 
+const getLogLevel = (env) => (env === "DEV" ? "debug" : "info");
+
+const logFileName = appConfig.LOGS_DIRECTORY + "/jumphost-%DATE%.log";
+
+const generateLogText = (info) => {
+  let transformedMessage = message.replace(/\n/g, " ").trim();
+
+  if (info instanceof Error) {
+    transformedMessage = `${info.message} ${info.stack}`;
+  }
+
+  return `[${timestamp}] ${level}: ${transformedMessage}`;
+};
+
 const logConfiguration = {
-  level: appConfig.ENVIRONMENT === "DEV" ? "debug" : "info",
+  level: getLogLevel(appConfig.ENVIRONMENT),
   transports: [
     new winston.transports.Console(),
     new DailyRotateFile({
-      filename: appConfig.LOGS_DIRECTORY + "/jumphost-%DATE%.log",
+      filename: logFileName,
       datePattern: "DD-MM-YYYY",
     }),
   ],
   format: winston.format.combine(
     winston.format.timestamp({ format: "DD-MM-YYYY HH:mm:ss" }),
-    winston.format.printf((info) => {
-      const { message, timestamp, level } = info;
-
-      let transformedMessage = message.replace(/\n/g, " ").trim();
-
-      if (info instanceof Error) {
-        transformedMessage = `${info.message} ${info.stack}`;
-      }
-      return `[${timestamp}] ${level}: ${transformedMessage}`;
-    })
+    winston.format.printf((info) => generateLogText(info))
   ),
 };
 const logger = winston.createLogger(logConfiguration);
 
-module.exports.logger = logger;
+module.exports = { logger, getLogLevel, generateLogText };
