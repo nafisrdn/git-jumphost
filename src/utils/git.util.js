62df65a17796f26d6af06ea2ebf85289d24f5564
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-20 14:50:20 +0700

    Add git utils to get multiple repos

--- src/utils/git.util.js
+++ src/utils/git.util.js
@@ -1,6 +1,15 @@
+const {
+  SOURCE_REPO_URL,
+  SOURCE_GIT_USERNAME,
+  SOURCE_GIT_PASSWORD,
+  TARGET_REPO_URL,
+  TARGET_GIT_USERNAME,
+  TARGET_GIT_PASSWORD,
+} = require("../config/git.config");
+
 const exec = require("child_process").exec;
 
-module.exports.runGitCommand = (command, useCwd = true) =>
+const runGitCommand = (command, useCwd = true) =>
   new Promise((resolve, reject) => {
     exec(
       `${command} 2>&1`,
@@ -14,17 +23,49 @@ module.exports.runGitCommand = (command, useCwd = true) =>
     );
   });
 
-module.exports.generateOriginUrlWithCreds = (
-  gitUsername,
-  gitPassowrd,
-  repoUrl
-) => `https://${gitUsername}:${gitPassowrd}@${repoUrl}`;
+const generateOriginUrlWithCreds = (gitUsername, gitPassowrd, repoUrl) =>
+  `https://${gitUsername}:${gitPassowrd}@${repoUrl}`;
 
-module.exports.getBranchName = (body) => {
+const getBranchName = (body) => {
   const { ref } = body;
 
   const branchIndex = ref.indexOf("heads/") + "heads/".length;
   const branchName = ref.substring(branchIndex);
 
   return branchName;
+};
+
+const getRepoLocalDirectory = (sourceRepoUrl, targetRepoUrl) =>
+  `${sourceRepoUrl.replace(/\//g, "-")}_${targetRepoUrl.replace(/\//g, "-")}`;
+
+const getReposInfo = () => {
+  const repos = [];
+
+  SOURCE_REPO_URL.split(",").forEach((sourceRepoUrl, index) => {
+    const repo = {
+      localRepoDirectoryName: getRepoLocalDirectory(
+        sourceRepoUrl,
+        TARGET_REPO_URL.split(",")[index]
+      ),
+      sourceRepoUrl,
+      sourceGitUsername: SOURCE_GIT_USERNAME.split(",")[index],
+      sourceGitPassword: SOURCE_GIT_PASSWORD.split(",")[index],
+
+      targetRepoUrl: TARGET_REPO_URL.split(",")[index],
+      targetGitUsername: TARGET_GIT_USERNAME.split(",")[index],
+      targetGitPassword: TARGET_GIT_PASSWORD.split(",")[index],
+    };
+
+    repos.push(repo);
+  });
+
+  return repos;
+};
+
+module.exports = {
+  runGitCommand,
+  generateOriginUrlWithCreds,
+  getBranchName,
+  getReposInfo,
+  getRepoLocalDirectory,
 };
