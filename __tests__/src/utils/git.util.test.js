f41bc55ef2db6234b2423c950ff91022d70eee79
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-02-23 16:10:45 +0700

    Refactor git.util.test.js

--- __tests__/src/utils/git.util.test.js
+++ __tests__/src/utils/git.util.test.js
@@ -1,25 +1,25 @@
-const exec = require("child_process").exec;
-
 const {
   runGitCommand,
   generateOriginUrlWithCreds,
   getBranchName,
 } = require("../../../src/utils/git.util");
+const childProcess = require("child_process");
 
-jest.mock("child_process", () => ({
-  exec: jest.fn(),
-}));
+jest.mock("child_process");
 
 describe("runGitCommand", () => {
+  beforeEach(() => {
+    childProcess.exec.mockClear();
+  });
+
   it("should call exec function with no cwd argument", async () => {
-    const mockExec = jest.requireMock("child_process").exec;
-    mockExec.mockImplementation((cmd, opt, callback) => {
+    childProcess.exec.mockImplementation((cmd, opt, callback) => {
       callback(null, "stdout", null);
     });
 
     await runGitCommand("git test command");
 
-    expect(exec).toHaveBeenCalledWith(
+    expect(childProcess.exec).toHaveBeenCalledWith(
       "git test command 2>&1",
       { cwd: null },
       expect.any(Function)
@@ -27,15 +27,13 @@ describe("runGitCommand", () => {
   });
 
   it("should call exec function with cwd argument", async () => {
-    const mockExec = jest.requireMock("child_process").exec;
-
-    mockExec.mockImplementation((cmd, opt, callback) => {
+    childProcess.exec.mockImplementation((cmd, opt, callback) => {
       callback(null, "stdout", null);
     });
 
     await runGitCommand("git test command", "/path/to/repo");
 
-    expect(exec).toHaveBeenCalledWith(
+    expect(childProcess.exec).toHaveBeenCalledWith(
       "git test command 2>&1",
       { cwd: "/path/to/repo" },
       expect.any(Function)
@@ -47,36 +45,23 @@ describe("runGitCommand", () => {
     expect(result).toBeInstanceOf(Promise);
   });
 
-  it("should resolve with stdout output on success", () => {
-    const mockExec = jest.requireMock("child_process").exec;
-
-    mockExec.mockImplementation((cmd, opt, callback) => {
-      callback(null, "stdout", null);
+  it("should resolve with ok output on success", async () => {
+    childProcess.exec.mockImplementation((cmd, opt, callback) => {
+      callback(null, "ok", null);
     });
 
-    expect(runGitCommand("git test command")).resolves.toBe("stdout");
+    const result = await runGitCommand("git test command");
+    expect(result).toBe("ok");
   });
 
-  test("test", async () => {
-    const mockExec = jest.requireMock("child_process").exec;
-
-    mockExec.mockImplementation((cmd, opt, callback) => {
+  it("should reject with error message on failure", async () => {
+    childProcess.exec.mockImplementation((cmd, opt, callback) => {
       callback(new Error("test error"), null, "error 2");
     });
 
-    expect(runGitCommand("git test command")).rejects.toBe(
+    await expect(runGitCommand("git test command")).rejects.toBe(
       "Error: test error \nstderr: error 2 \nstdout: null"
     );
-  });
-
-  xit("should reject with error message on failure", () => {
-    const mockExec = jest.requireMock("child_process").exec;
-
-    mockExec.mockImplementation((cmd, opt, callback) => {
-      callback(new Error("test error"), "s", "stderr");
-    });
-
-    expect(runGitCommand("git test command")).rejects.toThrow("a");
   });
 });
 
